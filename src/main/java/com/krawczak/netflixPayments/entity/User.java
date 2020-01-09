9ae3da4c6f703b241f7a404c034a349b585bb28f
Mikołaj Krawczak <mikolak25@gmail.com>
2020-01-09 18:57:01 +0100

    conecction to db ready

--- src/main/java/com/krawczak/netflixPayments/entity/User.java
+++ src/main/java/com/krawczak/netflixPayments/entity/User.java
@@ -1,5 +1,21 @@
 package com.krawczak.netflixPayments.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
 
 }
