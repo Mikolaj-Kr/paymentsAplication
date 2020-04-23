9655ce6fb96e22777d17584d21e0085f339829b7
Mikolaj-Kr <mikolak25@gmail.com>
2020-04-23 17:10:35 +0200

    changes defaults links

--- src/main/java/com/krawczak/netflixPayments/service/dotPayServices/CreateJson.java
+++ src/main/java/com/krawczak/netflixPayments/service/dotPayServices/CreateJson.java
@@ -25,8 +25,8 @@ public class CreateJson {
         json.put("control", control);
         json.put("language", "pl");
         json.put("ignore_last_payment_channel", 1);
-        json.put("url", "https://paymentmk.herokuapp.com/pay-payments");
-        json.put("urlc", "https://paymentmk.herokuapp.com/pay-urlc");
+        json.put("url", "https://dotpaytest.herokuapp.com/pay-payments");
+        json.put("urlc", "https://dotpaytest.herokuapp.com/pay-urlc");
         json.put("payer", payerJson);
         return json;
     }
